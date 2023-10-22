7149b7f5f61fba537b11b4552d9c491a084b41d4
Daniel Errington <derrington@gmail.com>
2023-10-22 13:57:55 +0100

    Add infinite scroll to gallery

--- site/src/components/Gallery.tsx
+++ site/src/components/Gallery.tsx
@@ -1,5 +1,6 @@
 import { ImageList, ImageListItem } from "@mui/material";
 import './Gallery.css';
+import { useEffect, useState } from "react";
 
 interface GalleryProps {
     photoNames: string[],
@@ -7,10 +8,20 @@ interface GalleryProps {
 
 export const Gallery = ({ photoNames }: GalleryProps) => {
 
+    const [photosShown, setPhotosShown] = useState(6);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if(photosShown < photoNames.length && window.scrollY + window.innerHeight * 2 > document.body.scrollHeight) {
+                setPhotosShown(value => value + 4);
+            }
+        });
+    }, [photoNames, setPhotosShown]);
+
     return (
         <ImageList>
             {
-                photoNames.map(photo => (
+                photoNames.slice(0, photosShown).map(photo => (
                     <ImageListItem key={photo}>
                         <img src={`/photos/thumbnails/${photo}`} className="galleryImage" onClick={() => document.location.href = `/photos/${photo}`} />
                     </ImageListItem>
